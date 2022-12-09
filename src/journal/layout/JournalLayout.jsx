77664b56279453d8ef2e77dba6a205cf4d595336
ariel <ariel.wm1@gmail.com>
2022-12-09 15:50:01 -0300

    adding layout navbar

---
+++ src/journal/layout/JournalLayout.jsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/system'
+import { NavBar } from '../components'
+
+const drawerWidth = 240
+
+export const JournalLayout = ({ children }) => {
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <NavBar drawerWidth={drawerWidth} />
+
+      {/* SideBar drawerWidth */}
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        {/* Toolbar */}
+        {children}
+      </Box>
+    </Box>
+  )
+}
--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,5 +1,16 @@
 import { Typography } from '@mui/material'
+import { JournalLayout } from '../layout/JournalLayout'
 
 export const JournalPage = () => {
-  return <Typography variant="h1">JournalPage</Typography>
+  return (
+    <JournalLayout>
+      <Typography>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Esse porro excepturi voluptatum
+        molestiae. Eius minima enim expedita, voluptates repellendus rerum tempora voluptate ut quae
+        consequatur incidunt nam quia vitae saepe!
+      </Typography>
+      {/* NothingSelected */}
+      {/* NoteView */}
+    </JournalLayout>
+  )
 }
